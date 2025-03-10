2a6c5b776d77b4dc9eb48edc57c212e1a6b2836d
Belik.AE <belik_anatoliy123@mail.ru>
2025-03-10 12:49:33 +1000

    Добавьте файлы проекта.

---
+++ Module_5_8_3/Main.cs
@@ -0,0 +1,22 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_5_8_3
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            var mainView = new View(commandData);
+            mainView.ShowDialog();
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ Module_5_8_3/ViewModel.cs
@@ -0,0 +1,64 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Plumbing;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using Prism.Commands;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_5_8_3
+{
+    public class ViewModel : BaseViewModel
+    {
+        public event EventHandler CloseRequest;
+        public event EventHandler HideRequest;
+        public event EventHandler ShowRequest;
+        private ExternalCommandData _commandData;
+        public DelegateCommand DelCommand { get; }
+
+
+        public ViewModel(ExternalCommandData commandData)
+        {
+            _commandData = commandData;
+            DelCommand = new DelegateCommand(Command);
+        }
+
+        private void Command()
+        {
+            UIApplication uiapp = _commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+
+            using (var ts = new Transaction(doc, "Transaction name"))
+            {
+                ts.Start();
+
+
+                ts.Commit();
+            }
+
+            RaiseCloseRequest();
+        }
+
+        private void RaiseCloseRequest()
+        {
+            CloseRequest?.Invoke(this, EventArgs.Empty);
+        }
+
+        private void RaiseHideRequest()
+        {
+            HideRequest?.Invoke(this, EventArgs.Empty);
+        }
+
+        private void RaiseShowRequest()
+        {
+            ShowRequest?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
